bdaf60e4fc31da6fa06c6291f366f0f2f9291b6b
Zigmantas Rackauskas <zigmantas@mrorangetech.se>
2018-12-17 22:21:42 +0200

    paskaitos medziaga

---
+++ classwork/JS/funkcijos/js/functions.js
@@ -0,0 +1,11 @@
+function getDivision(x, y){ // aprasomi funkcijos parametrai
+	return x/y;
+}
+
+function displayDivision(x, y){
+	var result = x/y;
+
+	console.log(result);
+	alert(result);
+	document.write(result);
+}
